23a4c5f7df13941ce6850cb802c596a3f20cda06
Rim21Tech <Rim21Tech@gmail.com>
2015-12-05 08:46:22 +1100

    Initial Commit

---
+++ segmentedSwitchTests/segmentedSwitchTests.swift
@@ -0,0 +1,36 @@
+//
+//  segmentedSwitchTests.swift
+//  segmentedSwitchTests
+//
+//  Created by Rim21 on 5/12/2015.
+//  Copyright © 2015 Nathan Rima. All rights reserved.
+//
+
+import XCTest
+@testable import segmentedSwitch
+
+class segmentedSwitchTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
